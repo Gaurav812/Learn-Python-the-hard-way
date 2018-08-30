c09a8d3c3c1accae31d44ccd1d9e6584feaec197
sushachawal <35626332+sushachawal@users.noreply.github.com>
2018-08-30 16:09:18 -0700

    Read this Gaurav

--- Test.py
+++ Test.py
@@ -2,3 +2,4 @@ print ("Hello world")
 print ("Gaurav smells")
 Excersizes 4-6 complete
 Sush is a poop
+Gaurav, you are supposed to save your python files into this directory and commit them to the repo
